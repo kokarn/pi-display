3cc8d4bf0190a9d51ea66fb415ce58e3419fac5b
Oskar Risberg <kokarn@gmail.com>
2016-07-15 08:35:42 +0200

    Minor improvement to custom items list

--- www/custom/index.php
+++ www/custom/index.php
@@ -1,6 +1,6 @@
 <?php
-    $data = array();
-    
+    $data = json_decode( file_get_contents( 'index.json' ) );
+
     header( 'Access-Control-Allow-Origin: *' );
     header( 'Content-Type: application/json' );
     die( json_encode( $data ) );
