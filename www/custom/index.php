f5e738b406f8ffb7fb42546cd4df6222834c5635
kokarn <kokarn@gmail.com>
2016-07-15 12:36:53 +0200

    Make sure custom returns something so we don't break stuff

--- www/custom/index.php
+++ www/custom/index.php
@@ -1,6 +1,10 @@
 <?php
-    $data = json_decode( file_get_contents( 'index.json' ) );
+$data = json_decode( @file_get_contents( 'index.json' ) );
 
-    header( 'Access-Control-Allow-Origin: *' );
-    header( 'Content-Type: application/json' );
-    die( json_encode( $data ) );
+if( !$data ){
+    $data = array();
+}
+
+header( 'Access-Control-Allow-Origin: *' );
+header( 'Content-Type: application/json' );
+die( json_encode( $data ) );
